ce7e00838bef541043e134c0ef8e65fde9edc61a
Gabor Parti <partigaborhk@gmail.com>
2023-06-30 08:55:50 +0800

    express update

--- main-express.py
+++ main-express.py
@@ -1,12 +1,10 @@
 print("\n###############################################\n### Initiating Parti's Primitive PDF Reader ###\n###############################################\n")
 
-print("Instructions:\n - Put a pdf in the same folder as this python file.\n - A quick, 5 question setup will follow regarding voice, speed, path, document, and page.\n - You can to input your settings or press enter for the default settings.\n\nDependencies: You will also need some python packages, run `pip install -r requirements.txt`.\nLimitations: Currently the only way to stop the engine is to kill the terminal.")
+print("Instructions:\n - Put a pdf in the same folder as this python file.\n\nDependencies: You will also need some python packages, run `pip install -r requirements.txt`.\nLimitations: Currently the only way to stop the engine is to kill the terminal.")
 
 # %pip install pyttsx3
 # %pip install PyPDF2
-# %pip install pygame # not needed
 
-# import easygui # not needed
 import os
 import sys
 import glob
@@ -16,7 +14,6 @@ import regex as re
 import time
 import PyPDF2
 import pyttsx3
-# from pygame import mixer # not needed
 
 ### Use auto-py-to-exe to make exe ###
 
@@ -25,66 +22,25 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 
 # voice
-# engine.setProperty('voice', voices[1].id)
-
-# select voice
-print("\n1. Select voice. Type 'f' or 'm' for a female or a male voice. (Hit enter for female.)")
-voice_choice = str(input("Please select voice (f/m): ")) or "f"
-
-if voice_choice == "m":
-  engine.setProperty('voice', voices[0].id)
-  print("\tOK, loading robotic male voice engine.")
-elif voice_choice == "f": 
-  engine.setProperty('voice', voices[1].id)
-  print("\tOK, loading robotic female voice engine.")
-else:
-  engine.setProperty('voice', voices[1].id)
-  print("\tOops, you have failed to hit any of the right keys, ... start again.")
-  exit()
-
-
+engine.setProperty('voice', voices[1].id)
 
 # speed
-# engine.setProperty('rate', 200)
-
-# select speed
-print("\n2. Select speed. Enter a value between 120 and 300 words per minute. (Hit enter for 200 wpm.)")
-speed_choice = input("Please enter speed: ") or "200"
-engine.setProperty('rate', int(speed_choice))
-print("\tOK, loading requested reading rate of", speed_choice, "wpm.")
-
-
-
-### Paths ###
+engine.setProperty('rate', 200)
 
 # relative path (current folder)
-# path = sys.path[0] # does not work with auto-py-to-exe
-
-# absolute path (local)
-print("\n3. Select files' directory.")
-path = sys.path[0]
-path_choice = input('Please enter path, such as "C:\\Users\\user\\read\\" (Hit enter for current folder.)') or path
-print(path)
+path = sys.path[0] # does not work with auto-py-to-exe, use "C:\\Users\\user\\...\\"
 
 # list of pdfs in relative  directory
-# pdfs = glob.glob("*.pdf")
+pdfs = glob.glob("*.pdf")
 
 # list of pdfs in absolute directory
-pdfs = []
-os.chdir(path)
-for file in glob.glob("*.pdf"):
-    pdfs.append(file)
-
-
+# pdfs = []
+# os.chdir(path)
+# for file in glob.glob("*.pdf"):
+#     pdfs.append(file)
 
 # file
-# book_choice = pdfs[0]
-
-# select file
-print("\n4. Please select a pdf. Enter the filename here, with extension. (Hit enter for the first pdf alphabetically)")
-book_choice = str(input("Please enter filename: ")) or pdfs[0]
-
-
+book_choice = pdfs[0]
 
 ### Parsing ###
 
@@ -96,13 +52,9 @@ print("\tParsing book:", path+"\\"+book_choice,"\n\tThis might take some time if
 # initialize dataframe to hold documents
 df = pd.DataFrame(columns=['page_no', 'page'])
 
-print("\t1/3...")
-
 # creating a pdf reader object 
 reader = PyPDF2.PdfReader(pdfFileObj, strict=False) 
 pages = len(reader.pages)
-
-print("\t2/3...")
 
 for p in range(pages):
   pageObj = reader.pages[p]
@@ -111,8 +63,6 @@ for p in range(pages):
   page_content = pageObj.extract_text()
   # print(page_content)
   df.loc[p, 'page'] = page_content
-
-print("\t3/3...")
 
 #closing the pdf file object 
 pdfFileObj.close() 
@@ -123,7 +73,7 @@ df.dropna(subset=['page'], inplace=True)
 df.reset_index(drop=True, inplace=True)
 
 # printing number of pages in pdf file 
-print("\tDropping empty ones... Done.\n\tTotal number of pages are", str(df.shape[0]))
+print("\tDropping empty ones... Done\n\tTotal number of pages are", str(df.shape[0]))
 time.sleep(1)
 print("\tCleaning some of the mess...")
 
@@ -140,14 +90,14 @@ df['page'] = [re.sub(r'\s+', " ", str(x)) for x in df['page']]
 
 content = " ".join(df['page'].tolist())
 
-print("\n5. Enter the page number you would like to continue, or hit enter to start from the beginning.")
-page_choice = input("Please enter page number: ") or "0"
-page_choice = int(page_choice)
-print("\tPage", str(page_choice), "selected, flipping to the page now...")
-time.sleep(1)
+# print("\nEnter the page number you would like to continue, or hit enter to start from the beginning.")
+# page_choice = input("Please enter page number: ") or "0"
+# page_choice = int(page_choice)
+# print("\tPage", str(page_choice), "selected, flipping to the page now...")
+# time.sleep(1)
 
-# cutoff before start
-df = df.iloc[page_choice: , :]
+# # cutoff before start
+# df = df.iloc[page_choice: , :]
 
 # estimate
 word_counts = df["page"].apply(lambda n: len(n.split()))
@@ -164,9 +114,6 @@ def convert_to_preferred_format(sec):
 
 print("\nEstimated time of the audio:", convert_to_preferred_format(seconds))
 time.sleep(1)
-
-# print("\nGo!\n")
-# df.head(20)
 
 #play
 for i, row in df.iterrows():
